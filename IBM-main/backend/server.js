d53a0101fcbe4d93fc84ab1155326deafc58cf34
Soorya A P <165376922+Soorya005@users.noreply.github.com>
2025-08-07 21:47:59 +0530

    Update server.js

--- IBM-main/backend/server.js
+++ IBM-main/backend/server.js
@@ -11,7 +11,7 @@ dotenv.config();
 connectDB();
 
 const app = express();
-const PORT = process.env.PORT || 0.0.0.0;
+const PORT = process.env.PORT || 5000;
 
 // Middleware (e.g., body parser)
 app.use(express.json());
